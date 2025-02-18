57db9d5757ba5745d7e4371fb25445eae381128f
gdrffg <gdrffg@gmail.com>
2025-02-18 18:10:32 +0900

    initial commit

---
+++ src/main/java/org/example/awsexampleproject/HomeController.java
@@ -0,0 +1,19 @@
+package org.example.awsexampleproject;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok().body("hello world");
+    }
+
+    @GetMapping("/healtCheck")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok().body("health check");
+    }
+}
---
+++ src/test/java/org/example/awsexampleproject/AwsExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.awsexampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsExampleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
